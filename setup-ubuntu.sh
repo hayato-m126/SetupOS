3e40376cadc9892d2f04c540bb4d08460c208e72
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2024-12-05 23:14:07 +0900

    fix: export path where uv installed
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,7 +6,7 @@ if ! (command -v curl >/dev/null 2>&1); then
 fi
 if ! (command -v uv >/dev/null 2>&1); then
     curl -LsSf https://astral.sh/uv/install.sh | sh
-    export PATH="$HOME/.cargo/bin:$PATH"
+    export PATH="$HOME/.local/bin"
 fi
 if ! (command -v ansible-playbook >/dev/null 2>&1); then
     uv tool install ansible-core
