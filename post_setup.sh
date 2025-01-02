a779369278899f53d0d5e62875eb103d4b7c8a9b
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2025-01-03 03:13:24 +0900

    fix: pre-commit
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

--- post_setup.sh
+++ post_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -f "$HOME/.ssh/github_rsa" ]; then
-    bash $HOME/.dotfiles/install.sh
+    bash "$HOME/.dotfiles/install.sh"
     mise install -y
 else
     echo "$HOME/.ssh/github_rsa not found"
 fi
 
-read -p "Do you want install python cli tools using uv? (y/n): " answer
+read -p -r "Do you want install python cli tools using uv? (y/n): " answer
 
 if [ "$answer" == "y" ]; then
     uv tool install pre-commit
