e94e08a2178d9670ca06b4d1000375861e3c713b
Hayato Mizushima <hayato-m126@users.noreply.github.com>
2025-01-11 16:02:56 +0900

    feat: split items performed by user (#40)
    * feat: split items
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * feat: add user setup
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * fix: empty playbook
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * fix: root user setup
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    * fix: pre-commit
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Hayato Mizushima <hayato-m126@users.noreply.github.com>

---
+++ user-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PLAYBOOK="ansible/user.yml"
+
+if ! (command -v uv >/dev/null 2>&1); then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    export PATH="$HOME/.local/bin"
+fi
+if ! (command -v ansible-playbook >/dev/null 2>&1); then
+    uv tool install ansible-core
+    ansible-galaxy collection install community.general
+fi
+
+ansible-playbook $PLAYBOOK --ask-become-pass
